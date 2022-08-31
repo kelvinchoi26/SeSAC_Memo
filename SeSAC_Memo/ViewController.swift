a76151047f39cc8a7375d72a95b38ab7c554d509
kelvinchoi26 <kelvinchoi@naver.com>
2022-08-31 16:34:30 +0900

    Initial Commit

---
+++ SeSAC_Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Memo
+//
+//  Created by 최형민 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
